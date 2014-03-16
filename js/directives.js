1c6541d41e2064361fad89764a4757ab97fd6254
mwcz <mwc@clayto.com>
2014-03-15 23:48:39 -0500

    latest changes from master

--- js/directives.js
+++ js/directives.js
@@ -4,30 +4,41 @@
 (function () {
     'use strict';
 
-    clayto.directive('keyLeft', function () {
+    var KEYS = {
+        37: 'Left',
+        38: 'Up',
+        39: 'Right',
+        40: 'Down'
+    };
+
+    function handle_keypress (keycode) {
         return function (scope, element, attrs) {
             element.bind("keydown keypress", function (event) {
-                if(event.which === 37) {
+                if(event.which === keycode) {
                     scope.$apply(function (){
-                        scope.$eval(attrs.keyLeft);
+                        var direction_name = KEYS[keycode];
+                        scope.$eval(attrs['key' + direction_name]);
                     });
                     event.preventDefault();
                 }
             });
         };
+    }
+
+    clayto.directive('keyUp', function () {
+        return handle_keypress(38);
+    });
+
+    clayto.directive('keyDown', function () {
+        return handle_keypress(40);
+    });
+
+    clayto.directive('keyLeft', function () {
+        return handle_keypress(39);
     });
 
     clayto.directive('keyRight', function () {
-        return function (scope, element, attrs) {
-            element.bind("keydown keypress", function (event) {
-                if(event.which === 39) {
-                    scope.$apply(function (){
-                        scope.$eval(attrs.keyRight);
-                    });
-                    event.preventDefault();
-                }
-            });
-        };
+        return handle_keypress(37);
     });
 
 }());
