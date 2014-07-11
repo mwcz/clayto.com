d8c7ce13f37797713654d19ba78a6cdbcbeea0cb
mwcz <mwc@clayto.com>
2014-07-11 15:56:32 -0500

    latest changes from master

--- js/services.js
+++ js/services.js
@@ -5,7 +5,7 @@
 
     var claytoServices = angular.module('clayto.services', ['ngResource']);
 
-    var flickr_clayto_photoset = 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=522454d4d32900b5b28dae598daab260&photoset_id=72157639704760704&format=json&nojsoncallback=1';
+    var flickr_clayto_photoset = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=522454d4d32900b5b28dae598daab260&photoset_id=72157639704760704&format=json&nojsoncallback=1';
 
     claytoServices.factory(
         'Photoset',
